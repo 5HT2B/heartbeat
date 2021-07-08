7e66ebbdde58eaa3db53b2dfe06111164567576b
lv <~@l1v.in>
2021-07-08 11:04:17 -0400

    fix: Wrong env var in `update.sh`

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,11 +12,11 @@ if [[ "$1" != "FIRST_RUN" ]]; then
   docker stop heartbeat || echo "Could not stop missing container heartbeat"
   docker rm heartbeat || echo "Could not remove missing container heartbeat"
 else
-  echo "MAKE SURE TO CREATE 'token' inside '$FOH_PATH'"
+  echo "MAKE SURE TO CREATE 'token' inside '$HB_PATH'"
 fi
 
 docker run --name heartbeat \
   -e ADDRESS="localhost:6011" \
-  --mount type=bind,source="$FOH_PATH",target=/heartbeat/config \
+  --mount type=bind,source="$HB_PATH",target=/heartbeat/config \
   --network host -d \
   l1ving/heartbeat
