b869412add40ec6277a872f188de3de6872d718f
lv <~@l1v.in>
2021-07-08 10:47:24 -0400

    cleanup: Simplify `update.sh`

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,10 +3,8 @@
 docker pull l1ving/heartbeat:latest
 
 if [[ "$1" != "FIRST_RUN" ]]; then
-  CONTAINER_ID="$(docker ps -f name=heartbeat --format "{{.ID}}" | head -n 1)"
-  echo "Stopping container $CONTAINER_ID"
-  docker stop "$CONTAINER_ID"
-  docker rm "$CONTAINER_ID"
+  docker stop heartbeat
+  docker rm heartbeat
 else
   echo "MAKE SURE TO CREATE 'token' inside '$HOME/heartbeat'"
 fi
